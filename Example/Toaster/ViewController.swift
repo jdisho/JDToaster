9e0f6d21da288dc0b8232eecd945d3a297d4a7ed
Joan Disho <dishojoan@gmail.com>
2019-06-12 11:19:04 +0200

    extend the example project

--- Example/Toaster/ViewController.swift
+++ Example/Toaster/ViewController.swift
@@ -15,18 +15,47 @@ class ViewController: UIViewController {
         view.backgroundColor = .white
 
         let shortToastButton = UIButton(type: .system)
-        shortToastButton.setTitle("Show Toaster", for: .normal)
-        shortToastButton.addTarget(self, action: #selector(showToaster), for: .primaryActionTriggered)
+        shortToastButton.setTitle("Short", for: .normal)
+        shortToastButton.addTarget(self, action: #selector(showShortToaster), for: .primaryActionTriggered)
 
-        view.addSubview(shortToastButton)
-        shortToastButton.translatesAutoresizingMaskIntoConstraints = false
+
+        let bigToastButton = UIButton(type: .system)
+        bigToastButton.setTitle("Big", for: .normal)
+        bigToastButton.addTarget(self, action: #selector(showBigToaster), for: .primaryActionTriggered)
+
+        let hugeToastButton = UIButton(type: .system)
+        hugeToastButton.setTitle("Huge", for: .normal)
+        hugeToastButton.addTarget(self, action: #selector(showHugeToaster), for: .primaryActionTriggered)
+
+        let dismissToastButton = UIButton(type: .system)
+        dismissToastButton.setTitle("Dismiss", for: .normal)
+        dismissToastButton.addTarget(self, action: #selector(dismissToaster), for: .primaryActionTriggered)
+
+        let stackView = UIStackView(arrangedSubviews: [shortToastButton, bigToastButton, hugeToastButton, dismissToastButton])
+        stackView.axis = .vertical
+        stackView.spacing = 16.0
+
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            shortToastButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            shortToastButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
         ])
     }
 
-    @objc private func showToaster() {
-       // Show Toaster
+    @objc private func showShortToaster() {
+       Toaster.default.show(text: "Hello Toast!")
+    }
+
+    @objc private func showBigToaster() {
+        Toaster.default.show(text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+    }
+
+    @objc private func showHugeToaster() {
+        Toaster.default.show(text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+    }
+
+    @objc private func dismissToaster() {
+        Toaster.default.dismiss(animated: true)
     }
 }
